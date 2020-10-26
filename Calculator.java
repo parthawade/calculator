92f3b151ded3e7657f8a6dd61763fb93210822c7
parthawade <73451192+parthawade@users.noreply.github.com>
2020-10-25 20:33:35 -0500

    Add files via upload
    added calculator class file

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+class Calculator {
+
+	public int add(int a, int b) {
+		int c = a+b;
+		return c;
+	}
+
+	public void addAndPrint(int a, int b) {
+		int c = a+b;
+		System.out.print("From addAndPrint(): "+c);
+	}
+
+	public static void main(String[] abc) {
+		Calculator calculator = new Calculator();
+		int sum = calculator.add(20,30);
+		System.out.println(sum);
+		calculator.addAndPrint(2424,87909);
+
+	}
+}
